b30c6ea8332e39c5fb787b51f30cd1e622eb5bae
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2019-04-04 11:14:51 +0100

    theora encoder not supported with LGPL build so remove ogg

--- test/test.ps1
+++ test/test.ps1
@@ -1,10 +1,10 @@
-$formats = @("wav","aac","ogg","mp4","mkv","flv","mov","avi")
+$formats = @("wav","aac","mp4","mkv","flv","mov","avi")
 foreach ($format in $formats) {
   $outfile = "test-muxing.$format"
   Echo "muxing to $outfile..."
   # muxing.exe writes to stderr, which powershell considers as a failure
   # so execute with cmd and redirect stderr within cmd to hide from powershell
-  cmd /c ".\muxing.exe $outfile" # > nul 2>&1"
+  cmd /c ".\muxing.exe $outfile > nul 2>&1"
   if (!(Test-Path $outfile)) {
     Echo "test failed: output file $outfile not found"
     $host.SetShouldExit(1)
