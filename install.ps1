42b386f939b79c9e4dccaf6e6b7e7f9f3495e119
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2019-04-04 13:44:41 +0100

    Prepare for debug and 32 bit builds.

--- install.ps1
+++ install.ps1
@@ -1,9 +1,17 @@
 $ffmpeg = "ffmpeg-20190327.0.1-681957b8"
 $version = ($ffmpeg -split '-')[1]
-$static32 = "$ffmpeg-lgpl21-v141-static-md-release-x86"
-$static64 = "$ffmpeg-lgpl21-v141-static-md-release-x64"
+$deb32 = "$ffmpeg-lgpl21-v141-static-md-debug-x86"
+$deb64 = "$ffmpeg-lgpl21-v141-static-md-debug-x64"
+$rel32 = "$ffmpeg-lgpl21-v141-static-md-release-x86"
+$rel64 = "$ffmpeg-lgpl21-v141-static-md-release-x64"
 $wc = New-Object System.Net.WebClient
 
-if (!(Test-Path "$static64.zip")) { $wc.DownloadFile("https://github.com/mcmtroffaes/ffmpeg-msvc-build/releases/download/$version/$static64.zip", "$env:temp/$static64.zip") }
+if (!(Test-Path "$deb32.zip")) { $wc.DownloadFile("https://github.com/mcmtroffaes/ffmpeg-msvc-build/releases/download/$version/$deb32.zip", "$env:temp/$deb32.zip") }
+if (!(Test-Path "$deb64.zip")) { $wc.DownloadFile("https://github.com/mcmtroffaes/ffmpeg-msvc-build/releases/download/$version/$deb64.zip", "$env:temp/$deb64.zip") }
+if (!(Test-Path "$rel32.zip")) { $wc.DownloadFile("https://github.com/mcmtroffaes/ffmpeg-msvc-build/releases/download/$version/$rel32.zip", "$env:temp/$rel32.zip") }
+if (!(Test-Path "$rel64.zip")) { $wc.DownloadFile("https://github.com/mcmtroffaes/ffmpeg-msvc-build/releases/download/$version/$rel64.zip", "$env:temp/$rel64.zip") }
 
-if (!(Test-Path $static64)) { Expand-Archive "$env:temp/$static64.zip" -DestinationPath . }
+if (!(Test-Path $deb32)) { Expand-Archive "$env:temp/$deb32.zip" -DestinationPath . }
+if (!(Test-Path $deb64)) { Expand-Archive "$env:temp/$deb64.zip" -DestinationPath . }
+if (!(Test-Path $rel32)) { Expand-Archive "$env:temp/$rel32.zip" -DestinationPath . }
+if (!(Test-Path $rel64)) { Expand-Archive "$env:temp/$rel64.zip" -DestinationPath . }
