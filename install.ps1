0758e26b5fa7a50829be567541934ab3e89c5693
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2019-04-23 07:58:16 +0100

    Update ffmpeg version to 20190422.0.0-eeca67e0.

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
-$ffmpeg = "ffmpeg-20190419.0.0-96fc0cbf"
+$ffmpeg = "ffmpeg-20190422.0.0-eeca67e0"
 $version = ($ffmpeg -split '-')[1]
 $deb32 = "$ffmpeg-lgpl21-v141-static-md-debug-x86"
 $deb64 = "$ffmpeg-lgpl21-v141-static-md-debug-x64"
