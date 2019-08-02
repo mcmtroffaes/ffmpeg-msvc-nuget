788c372c79cfaa2061da1a6ee3bb656709582109
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2019-08-02 07:58:41 +0100

    Update ffmpeg version to 20190729.0.0-43891ea8.

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
-$ffmpeg = "ffmpeg-20190715.0.0-7cb4f8c9"
+$ffmpeg = "ffmpeg-20190729.0.0-43891ea8"
 $version = ($ffmpeg -split '-')[1]
 $deb32 = "$ffmpeg-lgpl21-v141-static-md-debug-x86"
 $deb64 = "$ffmpeg-lgpl21-v141-static-md-debug-x64"
