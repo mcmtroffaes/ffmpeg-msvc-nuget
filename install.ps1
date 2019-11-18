d2bb9123493a38a0081304a9f3a00e946bed6096
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2019-11-18 13:19:07 +0000

    Update ffmpeg version to 20191117.0.0-b741a84.

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
-$ffmpeg = "ffmpeg-20191113.0.0-a7245ad"
+$ffmpeg = "ffmpeg-20191117.0.0-b741a84"
 $version = ($ffmpeg -split '-')[1]
 $deb32 = "$ffmpeg-lgpl21-v141-static-md-debug-x86"
 $deb64 = "$ffmpeg-lgpl21-v141-static-md-debug-x64"
