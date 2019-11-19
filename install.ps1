6414921989800a98dc780d9123935359feff6508
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2019-11-19 16:10:16 +0000

    Another fix, write download link to console to aid debugging.

--- install.ps1
+++ install.ps1
@@ -4,8 +4,14 @@ $rel32 = "$ffmpeg-lgpl21-v142-static-md-x86"
 $rel64 = "$ffmpeg-lgpl21-v142-static-md-x64"
 $wc = New-Object System.Net.WebClient
 
-if (!(Test-Path "$rel32.7z")) { $wc.DownloadFile("https://github.com/mcmtroffaes/ffmpeg-msvc-build/releases/download/$version/$rel32.zip", "$env:temp/$rel32.7z") }
-if (!(Test-Path "$rel64.7z")) { $wc.DownloadFile("https://github.com/mcmtroffaes/ffmpeg-msvc-build/releases/download/$version/$rel64.zip", "$env:temp/$rel64.7z") }
+if (!(Test-Path "$rel32.7z")) {
+  Write-Output "Downloading https://github.com/mcmtroffaes/ffmpeg-msvc-build/releases/download/$version/$rel32.7z ..."
+  $wc.DownloadFile("https://github.com/mcmtroffaes/ffmpeg-msvc-build/releases/download/$version/$rel32.7z", "$env:temp/$rel32.7z")
+}
+if (!(Test-Path "$rel64.7z")) {
+  Write-Output "Downloading https://github.com/mcmtroffaes/ffmpeg-msvc-build/releases/download/$version/$rel64.7z ..."
+  $wc.DownloadFile("https://github.com/mcmtroffaes/ffmpeg-msvc-build/releases/download/$version/$rel64.7z", "$env:temp/$rel64.7z")
+}
 
 if (!(Test-Path $rel32)) { 7z x "$env:temp/$rel32.7z" }
 if (!(Test-Path $rel64)) { 7z x "$env:temp/$rel64.7z" }
