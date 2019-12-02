63c195f7b46e80939a269cc66283f2510ffeebca
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2019-12-02 14:42:25 +0000

    Update ffmpeg version to 20191201.0.0-637742b.

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
-$ffmpeg = "ffmpeg-20191125.0.0-d5e3d8e"
+$ffmpeg = "ffmpeg-20191201.0.0-637742b"
 $version = ($ffmpeg -split '-')[1]
 $rel32 = "$ffmpeg-lgpl21-v142-static-md-x86"
 $rel64 = "$ffmpeg-lgpl21-v142-static-md-x64"
