7ec2fd7fa8cb2948637f1e928160bcb6bd2077b4
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2020-01-08 12:07:07 +0000

    Update ffmpeg version to 20200106.0.0-1e3f4b5.

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
-$ffmpeg = "ffmpeg-20200102.0.0-81172b5"
+$ffmpeg = "ffmpeg-20200106.0.0-1e3f4b5"
 $version = ($ffmpeg -split '-')[1]
 $rel32 = "$ffmpeg-lgpl21-v142-static-md-x86"
 $rel64 = "$ffmpeg-lgpl21-v142-static-md-x64"
