81ca344e7f0b6e34ef04999055077eaf03a9efec
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2020-08-04 08:28:30 +0100

    Simplify version logic (tie to ffmpeg-msvc-build version).

--- update-version.sh
+++ update-version.sh
@@ -16,24 +16,7 @@ then
 fi
 
 OLD_NUGET_VERSION=`sed -n 's|\s*<version>\(.*\)</version>|\1|p' FFmpeg.Nightly.LGPL.nuspec`
-OLD_NUGET_VERSION_MAJOR=`expr "${OLD_NUGET_VERSION}" : '^\([0-9]*\)[.]'`
-OLD_NUGET_VERSION_MINOR=`expr "${OLD_NUGET_VERSION}" : '^[0-9]*[.]\([0-9]*\)'`
-NEW_NUGET_VERSION_MAJOR=${NEW_VERSION:0:8}
-
-if [ "$OLD_NUGET_VERSION_MAJOR" \> "$NEW_NUGET_VERSION_MAJOR" ]
-then
-    echo "New major version $NEW_NUGET_VERSION_MAJOR is lower than old version $OLD_NUGET_VERSION_MAJOR."
-    exit 1
-fi
-
-if [ "$OLD_NUGET_VERSION_MAJOR" == "$NEW_NUGET_VERSION_MAJOR" ]
-then
-    NEW_NUGET_VERSION_MINOR=$((OLD_NUGET_VERSION_MINOR+1))
-else
-    NEW_NUGET_VERSION_MINOR=0
-fi
-
-NEW_NUGET_VERSION=${NEW_NUGET_VERSION_MAJOR}.${NEW_NUGET_VERSION_MINOR}.0
+NEW_NUGET_VERSION=`expr "${NEW_VERSION}" : '^\([0-9]*[.][0-9]*[.][0-9]*\)'`
 
 OLD_GIT_VERSION=`expr "${OLD_VERSION}" : '^[0-9.]*-\(.*\)'`
 NEW_GIT_VERSION=`expr "${NEW_VERSION}" : '^[0-9.]*-\(.*\)'`
