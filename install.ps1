e380ae43e8ccd0f91a1bf4766d36910703d46412
Matthias C. M. Troffaes <matthias.troffaes@gmail.com>
2020-08-04 08:28:33 +0100

    Update ffmpeg version to 20200730.2.0-134a48a.

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
-$ffmpeg = "ffmpeg-20200713.0.0-7772666"
+$ffmpeg = "ffmpeg-20200730.2.0-134a48a"
 $version = ($ffmpeg -split '-')[1]
 $rel32 = "$ffmpeg-lgpl21-v142-static-md-x86"
 $rel64 = "$ffmpeg-lgpl21-v142-static-md-x64"
